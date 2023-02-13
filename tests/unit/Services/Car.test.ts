c3b46f72d95a364d685ca48432e217081e553f9c
Camila Wolter <camila0wolter@gmail.com>
2023-02-13 19:28:28 -0300

    test: testes com 30% de cobertura da camada service

---
+++ tests/unit/Services/Car.test.ts
@@ -0,0 +1,92 @@
+import { expect } from 'chai';
+import { Model } from 'mongoose';
+import Sinon from 'sinon';
+import Car from '../../../src/Domains/Car';
+import ICar from '../../../src/Interfaces/ICar';
+import CarService from '../../../src/Services/CarService';
+
+describe('Testes da camada Service', function () {
+  it('Listagem de todos os carros com sucesso', async function () {
+    const listCars = [
+      {
+        id: '63eab2da6f1f836f7e7386ab',
+        model: 'Marea',
+        year: 2002,
+        color: 'Red',
+        status: true,
+        buyValue: 25.99,
+        doorsQty: 4,
+        seatsQty: 5,
+      },
+      {
+        id: '63eab3236f1f836f7e7386ad',
+        model: 'Fusca',
+        year: 2002,
+        color: 'Black',
+        status: true,
+        buyValue: 3.99,
+        doorsQty: 4,
+        seatsQty: 5,
+      },
+    ];
+
+    const domainCar = [
+      new Car(listCars[0]),
+      new Car(listCars[1]),
+    ];
+
+    Sinon.stub(Model, 'find').resolves(listCars);
+
+    const result = await new CarService().getAll();
+    expect(result).to.be.deep.equal(domainCar);
+  });
+
+  it('Listar somente um carro pelo seu ID', async function () {
+    const car = {
+      id: '63eab3236f1f836f7e7386ad',
+      model: 'Fusca',
+      year: 2002,
+      color: 'Black',
+      status: true,
+      buyValue: 3.99,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+
+    const domainCar = new Car(car);
+
+    Sinon.stub(Model, 'findById').resolves(domainCar);
+
+    const result = await new CarService().getById('63eab3236f1f836f7e7386ad');
+    expect(result).to.be.deep.equal(domainCar);
+  });
+
+  it('Adicionando um novo carro a lista', async function () {
+    const inputBody: ICar = {
+      model: 'Marea',
+      year: 2002,
+      color: 'Green',
+      status: true,
+      buyValue: 25.999,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+    const outputBody = {
+      id: '63eab2da6f1f836f7e7486ab',
+      model: 'Marea',
+      year: 2002,
+      color: 'Green',
+      status: true,
+      buyValue: 25.999,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+
+    const domainCar = new Car(outputBody);
+
+    Sinon.stub(Model, 'create').resolves(outputBody);
+
+    const result = await new CarService().create(inputBody);
+    expect(result).to.be.deep.equal(domainCar);
+  });
+});
