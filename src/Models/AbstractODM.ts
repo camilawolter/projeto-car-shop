e534bd4422569a4045482b82ccd62094b22a7672
Camila Wolter <camila0wolter@gmail.com>
2023-02-13 22:03:50 -0300

    refactor: refatoração das models e service

---
+++ src/Models/AbstractODM.ts
@@ -0,0 +1,43 @@
+import { 
+  model, 
+  Model, 
+  models, 
+  Schema,
+  UpdateQuery, 
+} from 'mongoose';
+
+abstract class AbstractODM<T> {
+  protected model: Model<T>;
+  protected schema: Schema;
+  protected modelName: string;
+
+  constructor(schema: Schema, modelName: string) {
+    this.schema = schema;
+    this.modelName = modelName;
+    this.model = models[this.modelName] || model(this.modelName, this.schema);
+  }
+
+  public async create(obj: T): Promise<T> {
+    return this.model.create({ ...obj });
+  }
+
+  public async find(): Promise<T[]> {
+    const vehicles = await this.model.find();
+    return vehicles;
+  }
+
+  public async findById(_id: string): Promise<T | null> {
+    const vehicleFound = await this.model.findById({ _id });
+    return vehicleFound;
+  }
+
+  public async update(_id: string, obj: Partial<T>): Promise<T | null> {
+    return this.model.findByIdAndUpdate(
+      { _id },
+      { ...obj } as UpdateQuery<T>,
+      { new: true },
+    );
+  }
+}
+
+export default AbstractODM;
--- src/Services/CarService.ts
+++ src/Services/CarService.ts
@@ -1,38 +1,35 @@
-import { Model } from 'mongoose';
 import ICar from '../Interfaces/ICar';
 import CarODM from '../Models/CarODM';
 import Car from '../Domains/Car';
 
 class CarService {
-  private model: Model<ICar>;
-
-  constructor() {
-    this.model = new CarODM().model;
-  }
-
   private createDomain(infos: ICar) {
     return new Car(infos);
   }
 
-  public async create(car: ICar | null): Promise<Car | null> {
-    const carCreate = await this.model.create(car);
+  public async create(car: ICar): Promise<Car | null> {
+    const carODM = new CarODM();
+    const carCreate = await carODM.create(car);
     return this.createDomain(carCreate);
   }
 
   public async getAll() {
-    const allCars = await this.model.find();
+    const carODM = new CarODM();
+    const allCars = await carODM.find();
     return allCars.map((car) => this.createDomain(car));
   }
 
   public async getById(id: string) {
-    const car = await this.model.findById({ _id: id });
+    const carODM = new CarODM();
+    const car = await carODM.findById(id);
     if (!car) throw new Error('Car not found');
     return this.createDomain(car);
   }
 
   public async update(id: string, infos: ICar) {
-    const carUpdate = await this.model.findByIdAndUpdate({ _id: id }, infos);
-    const newCar = await this.model.findById({ _id: id });
+    const carODM = new CarODM();
+    const carUpdate = await carODM.update(id, infos);
+    const newCar = await carODM.findById(id);
     if (newCar !== null) return this.createDomain(newCar);
     if (!carUpdate) throw new Error('Car not found');
   }
